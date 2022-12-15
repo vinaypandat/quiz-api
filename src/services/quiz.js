059452b20534f0098dc803c1807637f61e724821
Vinay Pandey <vinay.pandey@agiliosoftware.com>
2022-12-15 15:07:07 +0000

    players names uppercase

--- src/services/quiz.js
+++ src/services/quiz.js
@@ -35,7 +35,7 @@ async function getAllQuizzesAsync() {
     const list = quizzes.map(quiz => {
         return {
             id: quiz.id,
-            name: quiz.name
+            name: quiz.name.toUpperCase()
         };
     });
     return list;
